1d4a9a00d946ea6f22fada986d619796603d74a8
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-12-09 23:57:01 +0530

    feat: add custom cursor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ cursor: 'url("/icons/cursor.svg"), auto' }}>
       <body
         className={`${coolVeticaRegular.variable} ${brochilloRegular.variable} antialiased`}>
         {children}
